d216cda1e569d708a3a87f01298a6d7179a0c972
j3tjet <jesusbrere@gmail.com>
2022-10-27 16:30:06 -0400

    triangulos

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ function App() {
 const canvasRef = useRef(null);
 const ctxRef = useRef(null);
 var puedo=false,imgData;
-const [x,cambiarx]=useState(10);
-const [y,cambiary]=useState(10);
+let x1,y1;
+const [x,cambiarx]=useState(100);
+const [y,cambiary]=useState(100);
 const [dibujando, cambiarDibujando] = useState(false);
 const [grosor, cambiarGrosor] = useState(5);
 const [color, cambiarColor] = useState("black");
+const [relleno, cambiarRelleno] = useState("black");
 const [herramienta,cambiarHerramienta] = useState("lapiz");
 
 
@@ -20,27 +22,31 @@ useEffect(() => {
 	ctx.lineCap = "round";
 	ctx.lineJoin = "round";
 	ctx.strokeStyle = color;
+  ctx.fillStyle = relleno;
 	ctx.lineWidth = grosor;
 	ctxRef.current = ctx;
-}, [color,  grosor]);
+}, [color,  grosor,relleno]);
 
 const iniciarDibujo = (e) => {
   cambiarx(e.nativeEvent.offsetX);
   cambiary(e.nativeEvent.offsetY);
+  x1=e.nativeEvent.offsetX;
+  y1=e.nativeEvent.offsetY;
+  console.log(x1+"y"+y1);
   cambiarDibujando(true);
-  if(herramienta==="lapiz"){
-	ctxRef.current.beginPath();
+  ;
+  if(herramienta==="lapiz"){	
+    ctxRef.current.beginPath()
 	ctxRef.current.moveTo(e.nativeEvent.offsetX,e.nativeEvent.offsety);
-  console.log("x*" +x +"y="+y);
   }
+  if(herramienta==="triangulo"){	
+    }
 };
 
 const terminarDibujo = () => {
 	puedo=false;
 	cambiarDibujando(false);
-  if(herramienta==="lapiz"){
     ctxRef.current.closePath();
-  }
   
 };
 
@@ -54,11 +60,28 @@ const dibujar = (e) => {
     );
     ctxRef.current.stroke();}
     if(herramienta==="cuadrado"){
-      if(puedo){ctxRef.current.putImageData(imgData, 0, 0)}
+      if(puedo){ctxRef.current.putImageData(imgData, 0, 
+        0)}
       imgData = ctxRef.current.getImageData(0, 0, 1000, 700);
+      ctxRef.current.strokeRect(x,y,e.nativeEvent.offsetX-x,e.nativeEvent.offsetY-y);
       ctxRef.current.fillRect(x,y,e.nativeEvent.offsetX-x,e.nativeEvent.offsetY-y);
       puedo=true;
       console.log("????"+x+"????"+y)
+    }
+    if(herramienta==="triangulo"){
+      ctxRef.current.beginPath()
+      if(puedo){ctxRef.current.putImageData(imgData, 0, 0);}
+      imgData = ctxRef.current.getImageData(0, 0, 1000, 700);
+      
+      ctxRef.fillStyle="black";
+      ctxRef.current.moveTo(x,e.nativeEvent.offsetY);
+      ctxRef.current.lineTo(e.nativeEvent.offsetX,e.nativeEvent.offsetY);
+      ctxRef.current.lineTo(e.nativeEvent.offsetX - (e.nativeEvent.offsetX-x)/2,y);
+      ctxRef.current.fill();
+      ctxRef.current.closePath();      
+      ctxRef.current.stroke();
+      puedo=true;
+      console.log((e.nativeEvent.offsetX-x1)+"y"+y1);
     }
   
   
@@ -99,6 +122,7 @@ return (
     
     <Menu
 		cambiarColor={cambiarColor}
+    cambiarRelleno={cambiarRelleno}
 		cambiarGrosor={cambiarGrosor}
     limpiar={limpiar}
     cambiarHerramienta={cambiarHerramienta}
