33d6a7e919a771ea376d1aa57be6dd012bd80af4
JESUST <jesusbrere@gmail.com>
2022-11-11 15:06:12 -0400

    componente paletade colores

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import Paletas from "./Paletas";
 import "../App.css";
 
 
@@ -12,22 +13,9 @@ const Menu = ({ cambiarColor,cambiarRelleno, cambiarGrosor, limpiar, cambiarHerr
 
 	return (
 		<div className="Menu">
-			<div id="Colores">
-				<h1>Color</h1>
-				<button id="rojo" onClick={() => cambiarColor("red")}></button>
-				<button id="amarillo" onClick={() => cambiarColor("yellow")}></button>
-				<button id="verde" onClick={() => cambiarColor("green")}></button>
-				<button id="azul" onClick={() => cambiarColor("blue")}></button>
-				<button id="naranja" onClick={() => cambiarColor("orange")}></button>
-				<button id="negro" onClick={() => cambiarColor("black")}></button>
-			</div>
-			
-			<input
-				className="Colorea"
-				type="color"
-				onChange={(e) => {
-					cambiarColor(e.target.value);
-				}}
+			<Paletas
+			titulo={"Colores"}
+			cambio={cambiarColor}
 			/>
 			<h1>Grosor</h1>
 			<input
@@ -39,22 +27,11 @@ const Menu = ({ cambiarColor,cambiarRelleno, cambiarGrosor, limpiar, cambiarHerr
 					cambiarGrosor(e.target.value);
 				}}
 			/>
-			<div id="Colores">
-				<h1>Relleno</h1>
-				<button id="rojo" onClick={() => cambiarRelleno("red")}></button>
-				<button id="amarillo" onClick={() => cambiarRelleno("yellow")}></button>
-				<button id="verde" onClick={() => cambiarRelleno("green")}></button>
-				<button id="azul" onClick={() => cambiarRelleno("blue")}></button>
-				<button id="naranja" onClick={() => cambiarRelleno("orange")}></button>
-				<button id="negro" onClick={() => cambiarRelleno("black")}></button>
-			</div>
-			<input
-				className="Colorea"
-				type="color"
-				onChange={(e) => {
-					cambiarRelleno(e.target.value);
-				}}
+			<Paletas
+			titulo={"Relleno"}
+			cambio={cambiarRelleno}
 			/>
+
 			<div className="Botones">
 			<button onClick={() => cambiarColor("white")}>Borrador</button>
 			<button onClick={() => limpiar()}>Limpiar</button>
